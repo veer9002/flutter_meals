87c45047c543f9467808c6d8550dc064a237b982
Your Name <your.email@example.com>
2025-11-12 16:41:19 +0530

    initial theme setup

---
+++ lib/categories_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categories')),
+      body: const Center(child: Text('Categories Screen Content')),
+    );
+  }
+}
