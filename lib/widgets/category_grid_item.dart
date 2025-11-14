ae088853b1f6378c34fca7c763edeac76b3093ed
Your Name <your.email@example.com>
2025-11-14 13:38:31 +0530

    category screen inkwell widget added

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,15 +8,27 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+          selectionColor: Theme.of(context).colorScheme.onSurface,
         ),
       ),
     );
